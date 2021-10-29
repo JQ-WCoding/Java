531b1cdcc75c1f1a6cbfac1c81c96de3030b2ca4
JunQ <junlee36520620@gmail.com>
2021-10-29 13:37:38 +0900

    예제 공부

---
+++ src/practice/AntGame.java
@@ -0,0 +1,37 @@
+package practice;
+
+import java.util.ArrayList;
+
+public class AntGame {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add( 1 );
+        ArrayList<Integer> arr2;
+
+        System.out.println(arr);
+
+        for ( int i = 0; i < 10; i++ ) {
+            int count = 0;
+            arr2 = new ArrayList<>();
+            arr2.add( 1 );
+            int index = 0;
+
+            for ( int j = 0; j < arr.size(); j++ ) {
+                if ( arr.get( index ) == arr.get( j ) ){
+                    count++;
+                }else {
+                    arr2.add( count );
+                    arr2.add( arr.get( j ) );
+                    count = 1;
+
+                    index = j;
+                }
+            }
+
+            arr2.add( count );
+            arr = arr2;
+            System.out.println(arr);
+        }
+    }
+
+}
