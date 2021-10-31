358d8a433837e69df083f95ef3b85c458d58670a
SpellBound <junlee36520620@gmail.com>
2021-10-31 22:38:48 +0900

    baseball 게임 수정

---
+++ src/baseballGame/Gamer.java
@@ -0,0 +1,15 @@
+package baseballGame;
+
+public class Gamer {
+    private int[] num = new int[4];
+    boolean flag = false;
+
+    private void setNum(){
+
+    }
+
+    private boolean checkDuplicate(){
+        return false;
+    }
+
+}
