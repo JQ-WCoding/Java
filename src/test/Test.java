b77ff7d4e609ec58e0c67bc42d9c582d18e9ea0d
Jun_Q <junlee36520620@gmail.com>
2022-06-19 23:49:04 +0900

    Reverse

---
+++ src/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        Integer[] integer = new Integer[]{5, 2, 1, 4, 3};
+        List<Integer> arr = new ArrayList<>( List.of( integer ) );
+
+        List<Integer> collect = arr.stream()
+                .collect( ArrayList::new,
+                        (list, i) -> list.add( 0, i ),
+                        (result, input) -> result.addAll( 0, input ) );
+
+        System.out.println( "collect = " + collect );
+
+    }
+}
