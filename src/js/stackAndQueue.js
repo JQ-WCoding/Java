a45ad8d119cd3982692510979d6919837d997ea7
Jun_Q <junlee36520620@gmail.com>
2022-07-19 01:30:52 +0900

    stack & queue

---
+++ src/js/stackAndQueue.js
@@ -0,0 +1,15 @@
+printOne();
+setTimeout( printTwo, 1000 );
+printThree();
+
+function printOne() {
+    console.log( 1 );
+}
+
+function printTwo() {
+    console.log( 2  );
+}
+
+function printThree() {
+    console.log( 3 );
+}
