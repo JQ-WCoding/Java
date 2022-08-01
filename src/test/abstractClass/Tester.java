63f97f7d9ef935044859dfe3f093e9bd54c69baa
Jun_Q <junlee36520620@gmail.com>
2022-08-01 17:20:11 +0900

    injection 주입 가능

--- src/test/abstractClass/Tester.java
+++ src/test/abstractClass/Tester.java
@@ -1,8 +1,19 @@
 package test.abstractClass;
 
 public class Tester {
-    public static void main(String[] args) {
+    public static void main( String[] args ) {
         Sample sample1 = new SampleObject();
         Sample sample2 = new SampleObject2();
+
+        Sample s1 = inject1();
+        Sample s2 = inject2();
+    }
+
+    private static Sample inject1() {
+        return new SampleObject();
+    }
+
+    private static Sample inject2() {
+        return new SampleObject2();
     }
 }
