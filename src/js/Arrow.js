4a97b3f50ca86c133e6a384ac263d446e7f05b65
Jun_Q <junlee36520620@gmail.com>
2022-08-10 17:38:17 +0900

    arrow

--- src/js/Arrow.js
+++ src/js/Arrow.js
@@ -1,16 +1,24 @@
-const myArray = [ 'A', 'V' ];
+const myArray = ['A', 'V'];
 
-let arr = myArray.map( ( item ) => item );
+let arr = myArray.map((item) => item);
 
-console.log( arr );
+console.log(arr);
 
 const contacts = {
     familyName: 'Lee',
-    name: 'Cat',
-    age : 23
+    name      : 'Cat',
+    age       : 23,
 };
 
-let { familyName, name, age } = contacts;
-console.log( familyName );
-console.log( name );
-console.log( age );
+let {familyName, name, age} = contacts;
+
+console.log(contacts);
+
+for (let contactsKey in contacts) {
+    console.log(contactsKey);
+    console.log(Object.keys(contactsKey));
+}
+
+console.log(familyName);
+console.log(name);
+console.log(age);
