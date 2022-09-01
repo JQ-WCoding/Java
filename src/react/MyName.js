43f8eb46f800bbc2f6d3da40f9c15f4aec332329
Jun_Q <junlee36520620@gmail.com>
2022-09-01 22:06:38 +0900

    react 공부

--- src/react/MyName.js
+++ src/react/MyName.js
@@ -10,9 +10,10 @@ class MyName extends Component {
             <>
                 {/* jsx 내부에서 주석을 작성할 때는 이런 형식으로 주석을 작성함 */}
                 <div>
-                    안녕하세요! 제이름은 <b>{this.props.name}</b>입니다.
+                    안녕하세요! 제이름은 <b>{this.props.name}</b>입니다. {/**/}
                 </div>
             </>
+            /* 외부에서는 다시 주석을 이런식으로 답니다 */
         );
     }
 }
