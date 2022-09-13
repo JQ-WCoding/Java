b3924e0a4c92261ae88d48d82e9c35f1aceebc8a
Jun_Q <junlee36520620@gmail.com>
2022-09-13 13:41:45 +0900

    es6 module setting

---
+++ src/js/moduleSet.js
@@ -0,0 +1,15 @@
+const arr = [1, 2, 3];
+
+arr.forEach(console.log);
+
+let userId = 1;
+let userName = 'Lee';
+
+let user = {id: 1, name: 'Lee'};
+
+let users = [
+    {id: 1, name: 'Lee'}
+    , {id: 2, name: 'Kim'},
+];
+
+users.forEach(console.log);
