cc4b76e820dd31c06bfd1077a3990a059b92f79e
Jun_Q <junlee36520620@gmail.com>
2022-10-01 15:53:08 +0900

    array

--- src/js/objectTest.js
+++ src/js/objectTest.js
@@ -3,8 +3,10 @@ const array2 = [4, 5, 6];
 
 // new Version me
 let arrayTest = [...array1, ...array2];
+let arrayNew = [...array1, ...array2, 7, 8, 9];
 
 console.log( arrayTest );
+console.log( arrayNew );
 
 
 // old version me
