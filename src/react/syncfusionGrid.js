4fe8436fa6ca43314d30c535936ef397fca8191f
Jun_Q <junlee36520620@gmail.com>
2022-10-05 23:12:46 +0900

    react

---
+++ src/react/syncfusionGrid.js
@@ -0,0 +1,38 @@
+import {Component} from 'react';
+import ReactDom from 'react-dom';
+import React, {useState} from 'react';
+
+const test = {
+    inner1: 'hi',
+    inner2: 'react'
+}
+
+const [text, setText] = useState( test.inner1 );
+const [showText, setShowText] = useState( true );
+
+// const setInitialText = useCallback( ref => ref && setText( test.inner1 ) );
+
+const pressButton = () => {
+    let tagName = document.getElementsByTagName( 'h2' );
+    console.log( tagName );
+    setText( 'Pressed' );
+    setShowText( false );
+}
+
+class Day extends Component {
+    render() {
+        return <div>
+            <h1>{test.inner1}</h1>
+            <h2>{test.inner2}</h2>
+            <div>{text}</div>
+            <div>{showText}</div>
+
+            <button onClick={pressButton}>plus tags button</button>
+        </div>;
+    }
+}
+
+ReactDom.render(
+    <Day/>,
+    document.getElementById( 'root' ),
+);
