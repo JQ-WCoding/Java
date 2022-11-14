93cf58df96a561c949d254dfc935b7d54efc3a75
Jun_Q <junlee36520620@gmail.com>
2022-11-14 23:28:13 +0900

    sample

--- src/js/Arrow.js
+++ src/js/Arrow.js
@@ -1,8 +1,8 @@
 const myArray = ['A', 'V'];
 
-let arr = myArray.map((item) => item);
+let arr = myArray.map( ( item ) => item );
 
-console.log(arr);
+console.log( arr );
 
 const contacts = {
     familyName: 'Lee',
@@ -12,13 +12,21 @@ const contacts = {
 
 let {familyName, name, age} = contacts;
 
-console.log(contacts);
+console.log( contacts );
 
-for (let contactsKey in contacts) {
-    console.log(contactsKey);
-    console.log(Object.keys(contactsKey));
+for ( let contactsKey in contacts ) {
+    console.log( contactsKey );
+    console.log( Object.keys( contactsKey ) );
 }
 
-console.log(familyName);
-console.log(name);
-console.log(age);
+console.log( familyName );
+console.log( name );
+console.log( age );
+
+const sample = {
+    code   : 'sample',
+    style  : 'blur',
+    command: 'out'
+}
+
+const {code, style, command} = sample;
